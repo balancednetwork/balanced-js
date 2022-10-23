c1b284e320389dbc724cc8ad1b1ee07753e45dde
hetfly <hetfly@gmail.com>
2022-10-23 13:12:31 +0200

    feat: update define vote method

--- src/contracts/Governance.ts
+++ src/contracts/Governance.ts
@@ -83,17 +83,26 @@ export default class Governance extends Contract {
     return this.call(callParams);
   }
 
-  defineVote(name: string, description: string, vote_start: number, snapshot: number, actions: string) {
+  defineVote(name: string, description: string, vote_start: number, duration: number, forumLink: string, actions: string) {
     const callParams = this.transactionParamsBuilder({
       method: 'defineVote',
       params: {
         name: name,
         description: description,
         vote_start: IconConverter.toHex(vote_start),
-        snapshot: IconConverter.toHex(snapshot),
+        duration: IconConverter.toHex(duration),
+        forumLink: forumLink,
         actions: actions,
       },
     });
     return this.callICONPlugins(callParams);
+  }
+
+  getBalnVoteDefinitionCriterion() {
+    const callParams = this.paramsBuilder({
+      method: 'getBalnVoteDefinitionCriterion',
+    })
+
+    return this.call(callParams);
   }
 }
